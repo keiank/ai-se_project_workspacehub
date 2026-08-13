9ba171d3414b7b6af5356e1372ed1ab47b7558a0
Keian Kaserman <278810033+keiank@users.noreply.github.com>
2026-08-13 09:37:53 -0400

    Make eslint config simpler

--- eslint.config.js
+++ eslint.config.js
@@ -1,39 +1,26 @@
 const path = require('path');
+const { FlatCompat } = require('@eslint/eslintrc');
 
-// Import parser and plugins
-const tsParser = require('@typescript-eslint/parser');
-const tsPlugin = require('@typescript-eslint/eslint-plugin');
-const reactHooksPlugin = require('eslint-plugin-react-hooks');
-const reactRefreshPlugin = require('eslint-plugin-react-refresh');
-const prettierConfig = require('eslint-config-prettier');
-
-// Prefer the type-checked recommended config variants when available
-const tsTypeCheckedConfig =
-  tsPlugin.configs['recommended-requiring-type-checking'] ||
-  tsPlugin.configs['recommended-type-checked'] ||
-  tsPlugin.configs.recommended ||
-  {};
-const tsRules = tsTypeCheckedConfig.rules || {};
-
-const reactHooksRules = (reactHooksPlugin.configs && reactHooksPlugin.configs.recommended && reactHooksPlugin.configs.recommended.rules) || {};
-
-// Prettier rules (applied last to override style rules)
-const prettierRules =
-  (prettierConfig && prettierConfig.rules) ||
-  (prettierConfig && prettierConfig.configs && prettierConfig.configs.recommended && prettierConfig.configs.recommended.rules) ||
-  {};
+const compat = new FlatCompat({ baseDirectory: __dirname });
 
 module.exports = [
+  // Extend plugin configs (type-checked TS, react-hooks) and prettier last
+  ...compat.extends(
+    'plugin:@typescript-eslint/recommended-type-checked',
+    'plugin:react-hooks/recommended',
+    'prettier'
+  ),
+
   // Ignore common folders
   {
     ignores: ['**/node_modules/**', 'dist/**'],
   },
 
-  // Client (React) files: type-aware parsing and recommended type-checked rules
+  // Client (React) files: type-aware parsing
   {
     files: ['client/src/**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
-      parser: tsParser,
+      parser: require('@typescript-eslint/parser'),
       parserOptions: {
         project: [path.resolve(__dirname, 'client/tsconfig.app.json')],
         tsconfigRootDir: path.resolve(__dirname, 'client'),
@@ -42,40 +29,20 @@ module.exports = [
       },
     },
     plugins: {
-      '@typescript-eslint': tsPlugin,
-      'react-hooks': reactHooksPlugin,
-      'react-refresh': reactRefreshPlugin,
-    },
-    rules: {
-      ...tsRules,
-      ...reactHooksRules,
+      'react-refresh': require('eslint-plugin-react-refresh'),
     },
   },
 
-  // Server files: type-aware parsing and recommended type-checked rules
+  // Server files: type-aware parsing
   {
     files: ['server/src/**/*.{ts,tsx,js,jsx}'],
     languageOptions: {
-      parser: tsParser,
+      parser: require('@typescript-eslint/parser'),
       parserOptions: {
         project: [path.resolve(__dirname, 'server/tsconfig.json')],
         tsconfigRootDir: path.resolve(__dirname, 'server'),
         sourceType: 'module',
       },
-    },
-    plugins: {
-      '@typescript-eslint': tsPlugin,
-    },
-    rules: {
-      ...tsRules,
-    },
-  },
-
-  // Prettier: apply last to disable conflicting stylistic rules
-  {
-    files: ['**/*.{ts,tsx,js,jsx,css,scss,md,json}'],
-    rules: {
-      ...prettierRules,
     },
   },
 ];
